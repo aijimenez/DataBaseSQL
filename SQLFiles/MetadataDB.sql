b5a4f1419ffc0f57399226b5dfa24622c7194a8c
Keelji <kapucc@gmail.com>
2023-07-04 17:33:45 +0200

    metadata

--- SQLFiles/MetadataDB.sql
+++ SQLFiles/MetadataDB.sql
@@ -5,15 +5,27 @@ DESCRIBE accommodation;
 SELECT table_name, table_type, engine
 FROM information_schema.tables
 WHERE table_schema = 'sql_airbnb';
-ORDER BY table_name;
+-- ORDER BY table_name;
 
+-- Provides information about the database tables.
 SELECT
     TABLE_NAME, ENGINE, VERSION, ROW_FORMAT, TABLE_ROWS, AVG_ROW_LENGTH,
-    DATA_LENGTH, MAX_DATA_LENGTH, INDEX_LENGTH, DATA_FREE, AUTO_INCREMENT,
-    CREATE_TIME, UPDATE_TIME, CHECK_TIME, TABLE_COLLATION, CHECKSUM,
-    CREATE_OPTIONS, TABLE_COMMENT
+    DATA_LENGTH, INDEX_LENGTH, AUTO_INCREMENT,
+    CREATE_TIME, UPDATE_TIME, TABLE_COLLATION
   FROM INFORMATION_SCHEMA.TABLES
   WHERE table_schema = 'sql_airbnb';
 
 SHOW TABLE STATUS
   FROM sql_airbnb;
+
+-- Provides the size of the database
+SELECT
+SUM(ROUND(((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024), 2)) AS "SizeInMB"
+FROM INFORMATION_SCHEMA.TABLES
+WHERE
+TABLE_SCHEMA = "sql_airbnb";
+
+-- Provides the number of tables in the DB
+SELECT COUNT(*) AS NumberTables
+FROM information_schema.tables
+WHERE table_schema = 'sql_airbnb';
