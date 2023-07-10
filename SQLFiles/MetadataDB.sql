d35195635d89519819ec99bfbbd68659f7ead478
Keelji <kapucc@gmail.com>
2023-07-10 17:51:26 +0200

    Update Metadata

--- SQLFiles/MetadataDB.sql
+++ SQLFiles/MetadataDB.sql
@@ -1,23 +1,3 @@
-use sql_airbnb;
-SHOW tables;
-DESCRIBE accommodation;
-
-SELECT table_name, table_type, engine
-FROM information_schema.tables
-WHERE table_schema = 'sql_airbnb';
--- ORDER BY table_name;
-
--- Provides information about the database tables.
-SELECT
-    TABLE_NAME, ENGINE, VERSION, ROW_FORMAT, TABLE_ROWS, AVG_ROW_LENGTH,
-    DATA_LENGTH, INDEX_LENGTH, AUTO_INCREMENT,
-    CREATE_TIME, UPDATE_TIME, TABLE_COLLATION
-  FROM INFORMATION_SCHEMA.TABLES
-  WHERE table_schema = 'sql_airbnb';
-
-SHOW TABLE STATUS
-  FROM sql_airbnb;
-
 -- Provides the size of the database
 SELECT
 SUM(ROUND(((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024), 2)) AS "SizeInMB"
@@ -29,12 +9,10 @@ SELECT COUNT(*) AS NumberTables
 FROM information_schema.tables
 WHERE table_schema = 'sql_airbnb';
 
-SELECT *
-FROM INFORMATION_SCHEMA.TABLES
-WHERE table_schema = 'sql_airbnb'
-AND table_name='Accommodation';
-
-SELECT *
-FROM INFORMATION_SCHEMA.TABLES
-WHERE table_schema = 'sql_airbnb'
-AND table_name='Accommodation_KitchenAmenity';
+-- Provides information about the database tables.
+SELECT
+    TABLE_NAME, ENGINE, VERSION, ROW_FORMAT, TABLE_ROWS, AVG_ROW_LENGTH,
+    DATA_LENGTH, INDEX_LENGTH, AUTO_INCREMENT,
+    CREATE_TIME, UPDATE_TIME, TABLE_COLLATION
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE table_schema = 'sql_airbnb';
